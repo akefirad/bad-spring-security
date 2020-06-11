c76fbd4979201c372ba5c090f50d281a016705f1
akefirad <akefirad@gmail.com>
2020-06-11 19:43:17 +0100

    Initial commit

---
+++ src/main/java/com/example/MethodSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+}
